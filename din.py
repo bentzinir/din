de09db0c8a275efc2d962cf8cc2921217409472b
nir <bentzinir@gmail.com>
2017-11-02 15:17:54 +0200

    work in progress

--- din.py
+++ din.py
@@ -7,10 +7,9 @@ batch_initializer = {'gamma': init_ops.random_uniform_initializer}
 
 
 class DIN:
-    def __init__(self, num_actions, input_resolution, n_frames=4, num_layers=8, test_batch=1, is_training=True):
+    def __init__(self, num_actions, n_frames=4, num_layers=8, test_batch=1, is_training=True):
 
         self.num_actions = num_actions
-        self.input_resolution = input_resolution
         self.inplanes = 64
         self.n_frames = n_frames
         self.num_layers = num_layers
@@ -22,7 +21,7 @@ class DIN:
         # x = (?, 64, in_res, in_res)
         x = tf.layers.conv2d(x, filters=16, kernel_size=7, strides=2, padding='SAME',
                              kernel_initializer=torch_conv_initializer, bias_initializer=torch_conv_initializer,
-                             data_format="NCHW")
+                             data_format="channels_first")
         x = tf.contrib.layers.batch_norm(x, epsilon=0.00001, decay=0.9, scale=True, fused=True,
                                          data_format="NCHW", updates_collections=None,
                                          is_training=self.is_training, param_initializers=batch_initializer)
@@ -31,15 +30,15 @@ class DIN:
 
         # (?, 64, in_res, in_res)
         x = self._residual(x, 16, 'layer1')
-        x = tf.layers.max_pooling2d(x, 2, strides=2, data_format="NCHW")
+        x = tf.layers.max_pooling2d(x, 2, strides=2, data_format="channels_first")
         # (?, 64, in_res/2, in_res/2)
         x = self._residual(x, 16, 'layer2')
         x = self._residual(x, 16, 'layer3')
-        x = tf.layers.max_pooling2d(x, 2, strides=2, data_format="NCHW")
+        x = tf.layers.max_pooling2d(x, 2, strides=2, data_format="channels_first")
         # (?, 64, in_res/4, in_res/4)
         x = self._residual(x, 32, 'layer4')
         x = self._residual(x, 32, 'layer5')
-        x = tf.layers.max_pooling2d(x, 2, strides=2, data_format="NCHW")
+        x = tf.layers.max_pooling2d(x, 2, strides=2, data_format="channels_first")
         # (?, 64, in_res/8, in_res/8)
         x = self._residual(x, 64, 'layer6')
         x = self._residual(x, 64, 'layer7')
@@ -52,32 +51,41 @@ class DIN:
 
         return a_logits, d_logits
 
-    def _residual(self, x, planes, name, blocks=1, stride=1):
+    def _residual(self, input_tensor, planes, name, blocks=1, stride=1):
         with tf.variable_scope(name):
+            # downsample = None
+            # inplanes = x.get_shape().as_list()[1]
+            # if stride != 1 or inplanes != planes:
+            #     downsample = lambda y: tf.layers.conv2d(y, planes, data_format="channels_first",
+            #                                             strides=stride, kernel_size=1,
+            #                                             kernel_initializer=torch_conv_initializer,
+            #                                             bias_initializer=torch_conv_initializer)
+            # layers = [x]
+            # with tf.variable_scope("hg_0"):
+            #     layers.append(self._bottleneck(planes, layers[-1], stride, downsample))
+            # for i in range(1, blocks):
+            #     with tf.variable_scope("hg_" + str(i)):
+            #         layers.append(self._bottleneck(planes, layers[-1]))
+            # return layers[-1]
+            residual = input_tensor
+
+            inplanes = residual.get_shape().as_list()[1]
+
+            mid_channels = int(inplanes/2)
+
             downsample = None
-            if stride != 1 or self.inplanes != planes * self.expansion:
-                downsample = lambda y: tf.layers.conv2d(y, planes * self.expansion, data_format="NCHW",
+
+            if stride != 1 or inplanes != planes:
+                downsample = lambda y: tf.layers.conv2d(y, planes, data_format="channels_first",
                                                         strides=stride, kernel_size=1,
                                                         kernel_initializer=torch_conv_initializer,
                                                         bias_initializer=torch_conv_initializer)
-            layers = [x]
-            with tf.variable_scope("hg_0"):
-                layers.append(self._bottleneck(planes, layers[-1], stride, downsample))
-            self.inplanes = planes * self.expansion
-            for i in range(1, blocks):
-                with tf.variable_scope("hg_" + str(i)):
-                    layers.append(self._bottleneck(planes, layers[-1]))
-            return layers[-1]
 
-    def _bottleneck(self, midChannels, input_tensor, stride=1, downsample=None):
-        with tf.variable_scope("Bottleneck"):
-            residual = input_tensor
-
-            x = tf.contrib.layers.batch_norm(input_tensor, epsilon=0.00001, decay=0.9, scale=True, fused=True,
+            x = tf.contrib.layers.batch_norm(residual, epsilon=0.00001, decay=0.9, scale=True, fused=True,
                                              data_format="NCHW", updates_collections=None,
                                              is_training=self.is_training, param_initializers=batch_initializer)
             x = tf.nn.relu(x)
-            x = tf.layers.conv2d(x, midChannels, kernel_size=1, data_format="NCHW",
+            x = tf.layers.conv2d(x, mid_channels, kernel_size=1, data_format="channels_first",
                                  kernel_initializer=torch_conv_initializer,
                                  bias_initializer=torch_conv_initializer)
 
@@ -85,7 +93,7 @@ class DIN:
                                              data_format="NCHW", updates_collections=None,
                                              is_training=self.is_training, param_initializers=batch_initializer)
             x = tf.nn.relu(x)
-            x = tf.layers.conv2d(x, midChannels, kernel_size=3, strides=stride, data_format="NCHW",
+            x = tf.layers.conv2d(x, mid_channels, kernel_size=3, strides=stride, data_format="channels_first",
                                  padding='Same', use_bias=True,
                                  kernel_initializer=torch_conv_initializer,
                                  bias_initializer=torch_conv_initializer)
@@ -95,7 +103,44 @@ class DIN:
                                              is_training=self.is_training, param_initializers=batch_initializer)
 
             x = tf.nn.relu(x)
-            x = tf.layers.conv2d(x, 2 * midChannels, kernel_size=1, data_format="NCHW",
+            x = tf.layers.conv2d(x, planes, kernel_size=1, data_format="channels_first",
+                                 kernel_initializer=torch_conv_initializer,
+                                 bias_initializer=torch_conv_initializer)
+
+            if downsample is not None:
+                residual = downsample(input_tensor)
+
+            x += residual
+
+            return x
+
+    def _bottleneck(self, midChannels, input_tensor, stride=1, downsample=None):
+        with tf.variable_scope("Bottleneck"):
+            residual = input_tensor
+
+            x = tf.contrib.layers.batch_norm(input_tensor, epsilon=0.00001, decay=0.9, scale=True, fused=True,
+                                             data_format="NCHW", updates_collections=None,
+                                             is_training=self.is_training, param_initializers=batch_initializer)
+            x = tf.nn.relu(x)
+            x = tf.layers.conv2d(x, midChannels, kernel_size=1, data_format="channels_first",
+                                 kernel_initializer=torch_conv_initializer,
+                                 bias_initializer=torch_conv_initializer)
+
+            x = tf.contrib.layers.batch_norm(x, epsilon=0.00001, decay=0.9, scale=True, fused=True,
+                                             data_format="NCHW", updates_collections=None,
+                                             is_training=self.is_training, param_initializers=batch_initializer)
+            x = tf.nn.relu(x)
+            x = tf.layers.conv2d(x, midChannels, kernel_size=3, strides=stride, data_format="channels_first",
+                                 padding='Same', use_bias=True,
+                                 kernel_initializer=torch_conv_initializer,
+                                 bias_initializer=torch_conv_initializer)
+
+            x = tf.contrib.layers.batch_norm(x, epsilon=0.00001, decay=0.9, scale=True, fused=True,
+                                             data_format="NCHW", updates_collections=None,
+                                             is_training=self.is_training, param_initializers=batch_initializer)
+
+            x = tf.nn.relu(x)
+            x = tf.layers.conv2d(x, 2 * midChannels, kernel_size=1, data_format="channels_first",
                                  kernel_initializer=torch_conv_initializer,
                                  bias_initializer=torch_conv_initializer)
 
--- main.py
+++ main.py
@@ -16,140 +16,147 @@ def read_data():
     return x_batch, x_im_batch, ref_im_batch
 
 
-agent_buffer = Experience()
+def main():
+    conf = {'size': 10000}
 
+    agent_buffer = Experience(conf)
 
-# 1. create graph
-din = DIN()
 
+    # 1. create graph
+    din = DIN(num_actions=2)
 
-env = DoomClass(scenario='env/take_cover', timeout=1000, width=320, height=240, render=True)
 
+    env = DoomClass(scenario='env/take_cover', timeout=1000, width=320, height=240, render=True)
 
-opt = tf.train.AdamOptimizer(learning_rate=POLICY_LR)
 
+    opt = tf.train.AdamOptimizer(learning_rate=POLICY_LR)
 
-# Actor training graph
-trainable_vars = tf.trainable_variables()
 
+    # Actor training graph
+    trainable_vars = tf.trainable_variables()
 
-one_step_vars = [tf.Variable(tf.zeros_like(var.iniyialized_value()), trainable=False) for var in trainable_vars]
 
+    one_step_vars = [tf.Variable(tf.zeros_like(var.initialized_value()), trainable=False) for var in trainable_vars]
 
-accumed_vars = [tf.Variable(tf.zeros_like(var.iniyialized_value()), trainable=False) for var in trainable_vars]
 
+    accumed_vars = [tf.Variable(tf.zeros_like(var.initialized_value()), trainable=False) for var in trainable_vars]
 
-zero_accumed_op = [gv.assign(tf.zeros_like(gv)) for gv in accumed_vars]
 
+    zero_accumed_op = [gv.assign(tf.zeros_like(gv)) for gv in accumed_vars]
 
-zero_one_step_grad = [gv.assign(tf.zeros_like(gv)) for gv in one_step_vars]
 
+    zero_one_step_grad = [gv.assign(tf.zeros_like(gv)) for gv in one_step_vars]
 
-d_fake_ = tf.placeholder(shape=(None, 2), dtype=tf.float32)
 
+    d_fake_ = tf.placeholder(shape=(None, 2), dtype=tf.float32)
 
-if IS_TRAINING:
-    in_shape = [None, N_FRAMES, 240, 320]
-else:
-    in_shape = [1, N_FRAMES, 240, 320]
 
-x_fake = tf.placeholder(shape=in_shape, dtype=tf.float32, name="input")
+    if IS_TRAINING:
+        in_shape = [None, N_FRAMES*C_IN, 240, 320]
+    else:
+        in_shape = [1, N_FRAMES*C_IN, 240, 320]
 
+    x_fake = tf.placeholder(shape=in_shape, dtype=tf.float32, name="input")
 
-a_logits, d_fake = din.forward(x_fake)
 
+    a_logits, d_fake = din.forward(x_fake)
 
-pi = tf.nn.softmax(a_logits)
 
+    pi = tf.nn.softmax(a_logits)
 
-action = tf.multinomial(pi, BS)
 
+    action = tf.multinomial(pi, 1)
 
-uniform_logits = tf.ones_like(a_logits)
 
+    uniform_logits = tf.ones_like(a_logits)
 
-causal_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=uniform_logits, logits=a_logits)
 
+    causal_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=uniform_logits, logits=a_logits)
 
-actor_loss = tf.reduce_mean(tf.log(pi[action]) + BETA * causal_entropy)
 
+    actor_loss = tf.reduce_mean(tf.log(pi[action]) + BETA * causal_entropy)
 
-advantage = d_fake - d_fake_
 
+    advantage = d_fake - d_fake_
 
-one_step_gvs = opt.compute_gradients(loss=actor_loss)
 
+    one_step_gvs = opt.compute_gradients(loss=actor_loss)
 
-calc_one_step_grad = [one_step_vars[i].assign_add(gv[0]) for i, gv in enumerate(one_step_gvs)]
 
+    calc_one_step_grad = [one_step_vars[i].assign_add(gv[0]) for i, gv in enumerate(one_step_gvs)]
 
-score_one_step_grad = [one_step_vars[i].assign(advantage * one_step_vars[i]) for i, gv in enumerate(one_step_vars)]
 
+    score_one_step_grad = [one_step_vars[i].assign(advantage * one_step_vars[i]) for i, gv in enumerate(one_step_vars)]
 
-accum_grad = [accumed_vars[i].assign_add(gv[0]) for i, gv in enumerate(one_step_vars)]
 
+    accum_grad = [accumed_vars[i].assign_add(gv[0]) for i, gv in enumerate(one_step_vars)]
 
-apply_grad = opt.apply_gradients([(accumed_vars[i], one_step_vars[1]) for i, gv in enumerate(one_step_vars)])
 
+    apply_grad = opt.apply_gradients([(accumed_vars[i], one_step_vars[1]) for i, gv in enumerate(one_step_vars)])
 
-# Discriminator training graph
-x_expert = read_data("bla")
 
+    # Discriminator training graph
+    x_expert = read_data("bla")
 
-_, d_expert = din.forward(x_expert)
 
-d = tf.concat([d_expert, d_fake], axis=0)
+    _, d_expert = din.forward(x_expert)
 
-y = np.ones(x_expert.get_shape().as_list()[0])
+    d = tf.concat([d_expert, d_fake], axis=0)
 
-y_expert = tf.concat(values=[y, 1-y], axis=1)
+    y = np.ones(x_expert.get_shape().as_list()[0])
 
-y_fake = tf.concat(values=[1-y, 1], axis=1)
+    y_expert = tf.concat(values=[y, 1-y], axis=1)
 
-y = tf.concat([y_expert, y_fake])
+    y_fake = tf.concat(values=[1-y, 1], axis=1)
 
-discriminator_loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=d))
+    y = tf.concat([y_expert, y_fake])
 
-discriminator_grad_op = opt.minimize(discriminator_loss)
+    discriminator_loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=d))
 
-init_op = tf.global_variables_initializer()
+    discriminator_grad_op = opt.minimize(discriminator_loss)
 
+    init_op = tf.global_variables_initializer()
 
-with tf.Session() as sess:
-    sess.run(init_op)
 
-    for i in range(N_TRAIN_STEPS):
+    with tf.Session() as sess:
+        sess.run(init_op)
 
-        # train the discriminator
-        # x_expert = expert_buffer.sample()
+        for i in range(N_TRAIN_STEPS):
 
-        x_fake = agent_buffer.sample()
+            # train the discriminator
+            # x_expert = expert_buffer.sample()
 
-        sess.run(discriminator_grad_op, {})
+            x_fake = agent_buffer.sample()
 
-        # train the actor
-        trajectory = [env.game.new_episode()]
+            sess.run(discriminator_grad_op, {})
 
-        # set the environment at state s_t
-        env.initialize(trajectory[-1])
+            # train the actor
+            trajectory = [env.game.new_episode()]
 
-        sess.run(zero_accumed_op)
-        for _ in range(N_ACCUM_STEPS):
-            while not env.game.is_episode_finished():
-                sess.run(zero_one_step_grad)
-                # sample action from state s, load the gradient to gradient_vars
-                a, d_, _ = sess.run([action, d_fake, calc_one_step_grad], {din.input_tensor: trajectory[-N_FRAMES:]})
+            # set the environment at state s_t
+            env.initialize(trajectory[-1])
 
-                s_t = env.game.get_state()
+            sess.run(zero_accumed_op)
+            for _ in range(N_ACCUM_STEPS):
+                while not env.game.is_episode_finished():
+                    sess.run(zero_one_step_grad)
+                    # sample action from state s, load the gradient to gradient_vars
+                    a, d_, _ = sess.run([action, d_fake, calc_one_step_grad], {din.input_tensor: trajectory[-N_FRAMES:]})
 
-                env.game.make_action(a)
+                    s_t = env.game.get_state()
 
-                trajectory.append(s_t)
+                    env.game.make_action(a)
 
-                sess.run(score_one_step_grad, {din.input_tensor: trajectory[-N_FRAMES:], d_fake_: d_})
+                    trajectory.append(s_t)
 
-                sess.run(accum_grad)
+                    sess.run(score_one_step_grad, {din.input_tensor: trajectory[-N_FRAMES:], d_fake_: d_})
 
-        sess.run(apply_grad)
+                    sess.run(accum_grad)
 
-        agent_buffer.add(trajectory)
+            sess.run(apply_grad)
+
+            agent_buffer.add(trajectory)
+
+
+if __name__ == "__main__":
+    main()
